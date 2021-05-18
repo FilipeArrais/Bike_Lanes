1e56353bdae882b2d282d016b8fe813c976f7c5d
FilipeArrais <filipearrais5889@escolasdesatao.pt>
2021-05-18 22:57:00 +0100

    ja começa de funfar

--- Problem C.cpp
+++ Problem C.cpp
@@ -10,7 +10,7 @@ int n;
 vector<vector<int>> graph;
 vector<int> low;
 vector<int> dfs;
-int t = 0;
+int t = 1;
 stack<int> S;
 stack<stack<int>> Scc;
 vector<bool> containsStack;
@@ -33,23 +33,32 @@ int main() {
     for(int _ = 0; _ < num_casos; _++){
         cin >> n >> m >> q;
         graph = vector<vector<int>>(n+1,vector<int>(n+1));
-        low = vector<int>(n+1,-1);
+        low = vector<int>(n+1,0);
         dfs = vector<int>(n+1,-1);
-        containsStack = vector<bool>(n);
+        containsStack = vector<bool>(n+1,false);
     
         for(int __ = 0; __ < m; __++){
             cin >> A >> B >> D;
             graph[A][B] = D;
             //cout << A << " " << B << " " << D << "\n";    
         }
+        /*for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n ; j++){
+                if(graph[i][j] != 0){
+                  cout << graph[i][j]<<" ";   
+                }
+               
+            }
+            cout << "\n";
+        }*/
 
      
-        for(int i = 1; i < n ; i++){
+        for(int i = 1; i <= n ; i++){
             if(dfs[i] ==-1)
                 tarjan(i);
         }
         cout << conta_pois() <<"\n";
-        t = 0;
+        t = 1;
         S =  stack<int>();
         Scc =  stack<stack<int>>();
         
@@ -58,6 +67,7 @@ int main() {
     return 0;
 }
 
+//Acrescentar maior
 int conta_pois(){
     int count = 0;
     stack<int> aux;
@@ -68,6 +78,8 @@ int conta_pois(){
             count++;
         }
     }
+
+    
     return count;
 }
 
@@ -76,14 +88,19 @@ void tarjan(int v){
     t = t + 1;
     S.push(v);
     containsStack[v] = true;
-    for(auto w : graph[v]){
-        if(dfs[w] == -1){
-            tarjan(w);
-            low[w] = min(low[v], low[w]);
+    for(int w = 0; w <= n; w++){
+        if(graph[v][w] != 0){
+            //cout <<"vertice: "<< v <<"\n";
+            //cout << graph[w][v]<<"\n";
+            if(dfs[w] == -1){
+                tarjan(w);
+                low[v] = min(low[v], low[w]);
+            }
+            else if(containsStack[w]){
+                low[v] = min(low[v],dfs[w]);
+            }  
         }
-        else if(containsStack[w]){
-            low[v] = min(low[v],dfs[w]);
-        }
+        
     }
     if(low[v] == dfs[v]){
         stack<int> C;
